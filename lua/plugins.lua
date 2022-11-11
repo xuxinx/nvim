5588febf124e90b597e4836b66e5aa373d18753e
xuxin <xuxinmight@gmail.com>
2022-11-11 17:47:24 +0800

    change back to ultisnips

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -40,9 +40,9 @@ require'packer'.startup(function(use)
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
-            'hrsh7th/cmp-vsnip',
-            'hrsh7th/vim-vsnip',
-            'rafamadriz/friendly-snippets',
+            'SirVer/ultisnips',
+            'quangnguyen30192/cmp-nvim-ultisnips',
+            'honza/vim-snippets',
         },
         config = function ()
             require'x.cmp'
--- lua/x/cmp.lua
+++ lua/x/cmp.lua
@@ -5,14 +5,14 @@ vim.g.vsnip_snippet_dir = vim.fn.stdpath('config')..'/vsnip'
 cmp.setup({
     snippet = {
         expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
+            vim.fn["UltiSnips#Anon"](args.body)
         end
     },
     sources = require'cmp'.config.sources{
         { name = 'nvim_lsp' },
         { name = 'buffer' },
         { name = 'path' },
-        { name = 'vsnip' },
+        { name = 'ultisnips' },
     },
     mapping = require'cmp.config.mapping'.preset.insert()
 })
