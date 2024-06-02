f9a68cfb5ddf8b1a86b584861d7754c3411cc97f
xuxin <xuxinxsh@gmail.com>
2024-06-02 22:50:12 +0800

    hide all lsp sematic highlights

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -305,6 +305,11 @@ set_hls({
     -- ["@tag.delimiter"]                  = { link = "Delimiter" },  -- XML-style tag delimiters
 })
 
+-- hide all lsp semantic highlights
+for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+    vim.api.nvim_set_hl(0, group, {})
+end
+
 -- hop
 set_hls({
     HopNextKey = { link = "Search" },
