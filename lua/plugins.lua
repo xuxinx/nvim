169ea9b2a9dd6d4d14c08d6010163309f8bb3b1e
xuxin <xuxinxsh@gmail.com>
2025-03-07 15:57:47 +0800

    feat: update plugin configuration
    Update plugin configuration in lua/plugins.lua to improve functionality
    and organization.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -93,7 +93,7 @@ require("lazy").setup({
             {
                 "nvim-telescope/telescope-fzf-native.nvim",
                 build =
-                "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+                "make",
             },
         },
         config = function()
